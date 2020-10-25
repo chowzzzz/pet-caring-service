1940d3309fec95536a1023f16139e3a9061f3fac
Justin Tzuriel Krisnahadi <justin.tzuriel17@gmail.com>
2020-10-25 18:38:27 +0800

    Add input validation

--- app.js
+++ app.js
@@ -4,32 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-/* --- V7: Using dotenv     --- */
 require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
-/* --- V2: Adding Web Pages --- */
 var aboutRouter = require('./routes/about');
-/* ---------------------------- */
-
-/* --- V3: Basic Template   --- */
-var tableRouter = require('./routes/table');
-var loopsRouter = require('./routes/loops');
-/* ---------------------------- */
-
-/* --- V4: Database Connect --- */
 var selectRouter = require('./routes/select');
-/* ---------------------------- */
-
-/* --- V5: Adding Forms     --- */
-var formsRouter = require('./routes/forms');
-/* ---------------------------- */
-
-/* --- V6: Modify Database  --- */
 var signupRouter = require('./routes/signup');
-/* ---------------------------- */
 
 var app = express();
 
@@ -45,30 +26,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-/* --- V2: Adding Web Pages --- */
 app.use('/about', aboutRouter);
-/* ---------------------------- */
-
-/* --- V3: Basic Template   --- */
-app.use('/table', tableRouter);
-app.use('/loops', loopsRouter);
-/* ---------------------------- */
-
-/* --- V4: Database Connect --- */
 app.use('/select', selectRouter);
-/* ---------------------------- */
+app.use('/signup', signupRouter);
 
-/* --- V5: Adding Forms     --- */
-app.use('/forms', formsRouter);
-/* ---------------------------- */
-
-/* --- V6: Modify Database  --- */
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/signup', signupRouter);
-/* ---------------------------- */
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ public/javascripts/signupScript.js
@@ -0,0 +1,51 @@
+function check(event) {
+	// Get Values
+	var username  = document.getElementById('username').value.trim();
+	var name = document.getElementById('name').value.trim();
+	var email = document.getElementById('email').value.trim();
+	var password = document.getElementById('password').value.trim();
+	var address = document.getElementById('address').value.trim();
+
+	if (username.length > 20) {
+		alert("Username can only have up to 20 characters!");
+		event.preventDefault();
+		event.stopPropagation();
+		return false;
+	}
+	if (username.length == 0) {
+		alert("Invalid username!");
+		event.preventDefault();
+		event.stopPropagation();
+		return false;
+	}
+	if (name.length == 0) {
+		alert("Invalid name!");
+		event.preventDefault();
+		event.stopPropagation();
+		return false;
+	}
+	if (email.length == 0 || !email.includes("@")) {
+		alert("Invalid email!");
+		event.preventDefault();
+		event.stopPropagation();
+		return false;
+	}
+	if (password.length > 0 && password.length < 8) {
+		alert("Password must consist of at least 8 characters!");
+		event.preventDefault();
+		event.stopPropagation();
+		return false;
+	}
+	if (password.length == 0) {
+		alert("Invalid password!");
+		event.preventDefault();
+		event.stopPropagation();
+		return false;
+	}
+	if (address.length == 0) {
+		alert("Invalid address!");
+		event.preventDefault();
+		event.stopPropagation();
+		return false;
+	}
+}
